bbb99a222cbf4a3a8704107cc40e6e9393c510a8
Farhad Kazemi <farhad.kzm@gmail.com>
2018-01-17 23:39:22 +1100

    added appRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { ServiceComponent } from './service/service.component';
-import { ServiceListComponent } from './service/service-list/service-list.component';
-import { ServiceMapComponent } from './service/service-map/service-map.component';
-import { ServiceSearchComponent } from './service/service-search/service-search.component';
-import { ServiceNavComponent } from './service/service-nav/service-nav.component';
-import { ServiceInfoComponent } from './service/service-info/service-info.component';
-import { ProfileComponent } from './profile/profile.component';
-import { MyServicesComponent } from './my-services/my-services.component';
-import { NotificationComponent } from './notification/notification.component';
-import { MapItemInfoComponent } from './service/service-map/map-item-info/map-item-info.component';
-import { ListItemInfoComponent } from './service/service-list/list-item-info/list-item-info.component';
-import { MenuComponent } from './menu/menu.component';
+import {AppComponent} from './app.component';
+import {ServiceComponent} from './service/service.component';
+import {ServiceListComponent} from './service/service-list/service-list.component';
+import {ServiceMapComponent} from './service/service-map/service-map.component';
+import {ServiceSearchComponent} from './service/service-search/service-search.component';
+import {ServiceNavComponent} from './service/service-nav/service-nav.component';
+import {ServiceInfoComponent} from './service/service-info/service-info.component';
+import {ProfileComponent} from './profile/profile.component';
+import {MyServicesComponent} from './my-services/my-services.component';
+import {NotificationComponent} from './notification/notification.component';
+import {MapItemInfoComponent} from './service/service-map/map-item-info/map-item-info.component';
+import {ListItemInfoComponent} from './service/service-list/list-item-info/list-item-info.component';
+import {MenuComponent} from './menu/menu.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 
 
 import {
   MatButtonModule,
   MatCheckboxModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatTabsModule,
+  MatFormFieldModule,
+  MatInputModule,
   MatListModule,
-  MatFormFieldModule, MatSelectModule, MatInputModule
+  MatSelectModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatToolbarModule
 } from '@angular/material';
 
-import { AgmCoreModule } from '@agm/core';
+import {AgmCoreModule} from '@agm/core';
 
 
-import { FlexLayoutModule } from "@angular/flex-layout";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component: NotificationComponent},
+  {path: 'notifications', component: NotificationComponent},
+  {path: 'my_services', component: MyServicesComponent},
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -76,4 +85,5 @@ import { FlexLayoutModule } from "@angular/flex-layout";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
