49f9a7f79cdf5ba7f2eaa09f65205f05a424cd24
Farhad Kazemi <farhad.kzm@gmail.com>
2018-02-04 22:19:27 +1100

    added admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  bizColumns = ['name', 'type', 'address'];
+  bizDataSource = new MatTableDataSource(businessData);
+
+  userColumns = ['firstName', 'lastName', 'email'];
+  userDataSource = new MatTableDataSource(userData);
+
+  applyFilter(dataSource: MatTableDataSource<any[]>, filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    dataSource.filter = filterValue;
+  }
+
+
 }
+
+export interface Business {
+  address: string;
+  name: string;
+  type: string;
+}
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+const userData: User[] = [
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'restaurant'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'He'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Li'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Be'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'B'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'C'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'N'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'O'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'F'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Ne'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Na'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Mg'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Al'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Si'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'P'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'S'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Cl'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Ar'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'K'},
+  {firstName: 'Farhad', lastName: 'Kazemi', email: 'Ca'},
+];
+
+const businessData: Business[] = [
+  {address: '39 MT Alexander rd', name: 'Apadana Supermarket', type: 'restaurant'},
+  {address: '39 MT Alexander rd', name: 'Helium', type: 'He'},
+  {address: '39 MT Alexander rd', name: 'Lithium', type: 'Li'},
+  {address: '39 MT Alexander rd', name: 'Beryllium', type: 'Be'},
+  {address: '39 MT Alexander rd', name: 'Boron', type: 'B'},
+  {address: '39 MT Alexander rd', name: 'Carbon', type: 'C'},
+  {address: '39 MT Alexander rd', name: 'Nitrogen', type: 'N'},
+  {address: '39 MT Alexander rd', name: 'Oxygen', type: 'O'},
+  {address: '39 MT Alexander rd', name: 'Fluorine', type: 'F'},
+  {address: '39 MT Alexander rd', name: 'Neon', type: 'Ne'},
+  {address: '39 MT Alexander rd', name: 'Sodium', type: 'Na'},
+  {address: '39 MT Alexander rd', name: 'Magnesium', type: 'Mg'},
+  {address: '39 MT Alexander rd', name: 'Aluminum', type: 'Al'},
+  {address: '39 MT Alexander rd', name: 'Silicon', type: 'Si'},
+  {address: '39 MT Alexander rd', name: 'Phosphorus', type: 'P'},
+  {address: '39 MT Alexander rd', name: 'Sulfur', type: 'S'},
+  {address: '39 MT Alexander rd', name: 'Chlorine', type: 'Cl'},
+  {address: '39 MT Alexander rd', name: 'Argon', type: 'Ar'},
+  {address: '39 MT Alexander rd', name: 'Potassium', type: 'K'},
+  {address: '39 MT Alexander rd', name: 'Calcium', type: 'Ca'},
+];
