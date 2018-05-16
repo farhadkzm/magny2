09a8fb5f22469417c22314966ec6f503c8d290fc
Farhad Kazemi <fk231n@att.com>
2018-05-17 00:36:53 +1000

    added aws amplify

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from 'aws-amplify';
-//todo add actual configurtion and enable required aws features
-//todo follow https://aws.github.io/aws-amplify/media/quick_start#set-up-your-backend
 import aws_exports from './aws-exports';
 import 'hammerjs';
 
